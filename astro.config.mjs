c31ff2357bafd279dbebc560c9ac36fb2dcadb61
unknown <balint.leonard1@gmail.com>
2025-10-18 02:12:55 +0300

    chore: initial import + dark theme

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+import pagefind from "astro-pagefind";
+
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://leonard1403.github.io',
+	base: '/CyberRedBlog/',
+	integrations: [mdx(), sitemap(), pagefind()],
+});
