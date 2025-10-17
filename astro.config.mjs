6c222a2453eea4dcdeed03131b4d12929c7f2d78
unknown <balint.leonard1@gmail.com>
2025-10-18 02:53:04 +0300

    fix: base/site for GH Pages; restore BaseHead; dark palette; safe internal links

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://leonard1403.github.io',
-	base: '/CyberRedBlog/',
+	site: 'https://leonard1403.github.io/CyberRedBlog',
+	base: '/CyberRedBlog',
 	integrations: [mdx(), sitemap(), pagefind()],
 });
